599091423b9728ef68f2715cc3cf319cb0570604
Eric Ma <eric.ma@modernatx.com>
2021-10-22 13:11:04 -0400

    Functional reorganization based on install priority.
    - Determine whether no installation is necessary (no_install).
    - Try to install via dpkg (debian_install).
    - Try to install via brew (brew_install).
    - Raise warning to install on their own (warning_install).

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import os
-import requests
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-from distutils.command.install import install as orig_install
 import subprocess
+import warnings
+from distutils.command.install import install as orig_install
+from pathlib import Path
+
+import requests
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+SUCCESS_RETURN_CODE = 0
 
 
 class InstallWrapper(install):
@@ -11,9 +16,10 @@ class InstallWrapper(install):
 
     The order of priority in which we try:
 
-    - Try to install via dpkg.
-    - Try to install via brew.
-    - Raise warning to install on their own.
+    - Determine whether no installation is necessary (no_install).
+    - Try to install via dpkg (debian_install).
+    - Try to install via brew (brew_install).
+    - Raise warning to install on their own (warning_install).
 
     :sa: https://www.anomaly.net.au/blog/running-pre-and-post-install-jobs-for-your-python-packages/
     :sa: https://stackoverflow.com/questions/21915469/python-setuptools-install-requires-is-ignored-when-overriding-cmdclass
@@ -25,25 +31,20 @@ class InstallWrapper(install):
         # :sa: https://github.com/automl/random_forest_run/commit/f975f947979cd1bc46eca30833f8267c2f71514c
         # install.do_egg_install(self)
         orig_install.run(self)
-        # install the non-python binaries by Debian
-        result = dpkg_install()
-        if result.returncode != 0:
+        result = no_install()
+        if result.return_code != SUCCESS_RETURN_CODE:
+            result = debian_install()
+        if result.returncode != SUCCESS_RETURN_CODE:
+            print("Debian-based installation failed. Trying with brew instead...")
             result = brew_install()
-        if result.returncode != 0:
+        if result.returncode != SUCCESS_RETURN_CODE:
             warning_install()
 
 
-def warning_install():
-    """Raise a warning that none of the previous installation methods worked."""
-    import warnings
-
-    warnings.warn(
-        "Installation by dpkg and brew failed. "
-        "Please manually install MAFFT to enjoy SeqLike's Series Accessor's "
-        "method-chained alignment capabilities. \n\n"
-        "MAFFT installation instructions can be found on "
-        "https://mafft.cbrc.jp/alignment/software/."
-    )
+def no_install():
+    """Run `which mafft` to see if mafft is already installed."""
+    result = subprocess.run(["which", "mafft"])
+    return result
 
 
 def brew_install():
@@ -54,45 +55,50 @@ def brew_install():
     return result
 
 
-def dpkg_install():
-    """Install via dpkg."""
+def debian_install():
+    """Install MAFFT on debian."""
     result = subprocess.run(["apt-get", "update"])
-    if result.returncode == 0:
+    if result.returncode == SUCCESS_RETURN_CODE:
         result = subprocess.run(["apt-get", "install", "-y", "ghostscript"])
-    if result.returncode == 0:
-        result = download_and_install_mafft(kind="deb")
+    if result.returncode == SUCCESS_RETURN_CODE:
+        deb_filepath = download_mafft(kind="deb")
+        print(f"Installing {deb_filepath}...")
+        result = subprocess.run(["dpkg", "--install", deb_filepath])
     return result
 
 
-def yum_install():
-    """Placeholder for installation via yum.
-
-    NOTE: Though I've used RedHat and CentOS, I'm not familiar enough with the commands.
-    (@ericmjl)
-    """
-    pass
+def rpm_install():
+    """Install MAFFT using RPM."""
+    raise NotImplementedError("Installation by RPM not yet supported.")
 
 
-def download_and_install_mafft(kind="deb"):
-    """Install MAFFT as a Debian package.
+def warning_install():
+    """Raise a warning that none of the previous installation methods worked."""
+    warnings.warn(
+        "Installation by dpkg and brew failed. "
+        "Please manually install MAFFT to enjoy SeqLike's Series Accessor's "
+        "method-chained alignment capabilities. \n\n"
+        "MAFFT installation instructions can be found on "
+        "https://mafft.cbrc.jp/alignment/software/."
+    )
 
-    Default to Debian install.
-    """
-    mafft_filenames = dict(rpm="mafft-7.487-gcc_fc6.x86_64.rpm", deb="mafft_7.487-1_amd64.deb")
-    mafft_file = mafft_filenames[kind]
-    deb_filepath = "/tmp/" + mafft_file
 
-    # download the package from mafft.cbrc.jp
+def download_mafft(kind="deb") -> Path:
+    """Download MAFFT from the original website."""
+    if kind == "deb":
+        mafft_file = "mafft_7.487-1_amd64.deb"
+    elif kind == "rpm":
+        mafft_file = "mafft-7.487-gcc_fc6.x86_64.rpm"
+    else:
+        raise NameError("`kind` should be one of 'deb' or 'rpm'.")
+    deb_filepath = Path("/tmp/") / mafft_file
     if not os.path.isfile(deb_filepath):
         url = "https://mafft.cbrc.jp/alignment/software/" + mafft_file
         print("Downloading %s..." % url)
         r = requests.get(url, allow_redirects=True)
         with open(deb_filepath, "wb") as f:
             f.write(r.content)
-
-    # install the MAFFT package by dpkg
-    print("Installing %s..." % deb_filepath)
-    return subprocess.run(["dpkg", "--install", deb_filepath])
+    return deb_filepath
 
 
 setup(
