bde544fc3654801001115e3b90fc36e267aa5d26
Sugato Ray <sugatoray@users.noreply.github.com>
2022-01-17 19:13:21 -0600

    Update setup.py
    - keep version as `X.Y.Z`
    
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ install_requires = [
 
 setup(
     name="seqlike",
-    version="1.1.7.dev1",
+    version="1.1.6",
     packages=find_packages(),  # https://stackoverflow.com/a/22442340
     cmdclass={"install": InstallWrapper},
     long_description=README,
