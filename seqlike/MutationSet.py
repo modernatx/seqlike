f7dfba625032d98a2bcb98211400ec84737022d2
Eric Ma <ericmjl@users.noreply.github.com>
2022-10-04 18:23:27 +0000

    Add docstrings across MutationSet.py

--- seqlike/MutationSet.py
+++ seqlike/MutationSet.py
@@ -3,21 +3,31 @@
 from copy import deepcopy
 from multipledispatch import dispatch
 from typing import Iterable, Union, List
-from .Mutation import Mutation, magical_parse
+from .Mutation import Mutation
 
 
 class MutationSet(list):
+    """MutationSet class definition.
+
+    MutationSets are an iterable container around Mutations.
+    The Mutations inside a MutationSet are orderable, hence this class inherits from lists.
+    """
+
     def __init__(self, mutations: Iterable[Union[Mutation, str]]):
+        """Initialize a MutationSet.
+
+        MutationSets are initializable from an iterable of Mutations or strings.
+        """
         # Parse a list of mutation strings:
         parsed_mutations = []
-        for m in mutations:
+        for i, m in enumerate(mutations):
             if isinstance(m, str):
-                parsed_mutations.append(magical_parse(m))
+                parsed_mutations.append(Mutation(m))
             elif isinstance(m, Mutation):
                 parsed_mutations.append(m)
             else:
                 raise ValueError(
-                    f"Mutations must be an iterable of mutation strings or Mutations. Element {m} violates this assumption!"
+                    f"Mutations must be an iterable of mutation strings or Mutations. Element {m} at position {i} violates this assumption!"
                 )
         self.mutations = sorted(parsed_mutations)
 
@@ -33,7 +43,7 @@ class MutationSet(list):
         return positions
 
     def __add__(self, other):
-        """Only defined for integers for now, should be defined for other mutation sets!"""
+        """Add stuff to mutations."""
         # self.mutations = [m + other for m in self.mutations]
         return _add(self, other)
 
@@ -50,24 +60,43 @@ class MutationSet(list):
         return next(self.mutations)
 
     def to_str(self):
-        """Convenience method to convert the MutationSet object into a string representation."""
+        """Convert the MutationSet object into a string.
+
+        This is a convenience method to help match the API of SeqLikes,
+        which also have a `.to_str()` method.
+        """
         return self.__str__()
 
 
 @dispatch(MutationSet, int)
 def _add(obj: MutationSet, other: int):
-    """Add an integer to a MutationSet."""
+    """Offset all Mutations' positions in a MutationSet by an integer amount.
+
+    This enables us to write:
+
+    ```python
+    ms = MutationSet(...)
+    offset_ms = ms + 1
+    ```
+
+    instead of:
+
+    ```python
+    offset_ms = MutationSet([m + 1 for m in ms])
+    """
     mutations = [deepcopy(m) + other for m in obj.mutations]
     return MutationSet(mutations=mutations)
 
 
 @dispatch(MutationSet, MutationSet)
 def _add(obj: MutationSet, other: MutationSet):
-    """Add a MutationSet to another MutationSet."""
+    """Add a MutationSet to another MutationSet.
+
+    Add two MutationSets together.
+    """
     new_mutations = deepcopy(obj.mutations)
     for mutation in other.mutations:
-        if mutation.position not in obj.positions:
-            new_mutations.append(mutation)
+        new_mutations.append(mutation)
 
     new_mutations = sorted(new_mutations)
 
